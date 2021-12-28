2ca5a4c15e3d08e257f1907aac442d63fd687c20
Tzon22231 <96737364+Tzon22231@users.noreply.github.com>
2021-12-28 21:51:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *")
 header("content-type : application/json")
-
+§get JSON("https://raw.githubusercontent.com/Tzon22231/Json-Anmeldung-2/main/rs.json", function(json){});
 ?>
 
