4ada1b95e602b9f841a28d1be907941f07d615d7
Tzon22231 <96737364+Tzon22231@users.noreply.github.com>
2022-01-01 22:33:27 +0100

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *")
-header("content-type : application/json")
-§get JSON("https://raw.githubusercontent.com/Tzon22231/Json-Anmeldung-2/main/rs.json", function(json){});
-?>
-
